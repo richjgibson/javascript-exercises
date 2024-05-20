b405fdb334fca44428900e6c367cf22d19980c43
Rich Gibson <richgibson86@gmail.com>
2024-05-20 21:24:22 +0100

    Working with one

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, toRemove) {
+	// const index = array.indexOf(toRemove);
+	let toReturn = [];
+	for (let i = 0; i < array.length; i++) {
+		if(array[i] != toRemove) {
+			toReturn.push(array[i]);
+		}
+	}
+	return toReturn;
 };
 
 // Do not edit below this line
